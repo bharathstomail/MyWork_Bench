907304ed8cea83bc173bdb665fb3896ea20b8f27
HSBHARA <HSBHARA@CGSCLX052353091.inedc.corpintra.net>
2018-08-23 16:25:24 +0530

    test

---
+++ Test_Proj/Testing/test.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 23, 2018
+
+@author: HSBHARA
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
